8b5939846c08083ae71ab05d44fc964b89b52e63
rirakib <rirrakib10@gmail.com>
2023-09-10 07:35:09 -0700

    basic typescript

---
+++ basic/index.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(10, 12);
+console.log(result);
---
+++ basic/index.ts
@@ -0,0 +1,8 @@
+
+function sum(a: number,b: number) : number {
+    return a+b
+}
+
+let result = sum(10,12)
+
+console.log(result)
