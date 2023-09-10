0fc94c8090eb60fbe289e7aff58583fce25dd25a
rirakib <rirrakib10@gmail.com>
2023-09-10 08:09:28 -0700

    data-type

--- data-type/index.js
+++ data-type/index.js
@@ -2,9 +2,29 @@
 var username;
 var age;
 var isActive;
+//union data type
+var reference;
 username = "rakib";
 age = 25;
 isActive = true;
+reference = "ac-" + Math.floor(Math.random() * 1000);
+console.log(reference);
+//array data-type 
+var fruites;
+fruites = ['apple', 'mango', 'goava'];
+console.log(fruites);
+var marks;
+marks = [25, 45, 50, 35];
+console.log(marks);
+var mixed;
+mixed = ['rakib', true, 24];
+console.log(mixed);
+//union way
+var stringOrNumber;
+stringOrNumber = ['rakib', 22];
+//tupel way
+var tupleStringOrNumber;
+tupleStringOrNumber = ['riyad', 22];
 //void
 function hello() {
     console.log('hello world');
--- data-type/index.ts
+++ data-type/index.ts
@@ -1,12 +1,39 @@
 //built in data type 
-
 let username : string
 let age : number
 let isActive : boolean
 
+//union data type
+let reference : string | number
+
 username ="rakib"
 age = 25
-isActive = true 
+isActive = true
+reference = "ac-"+Math.floor(Math.random() * 1000)
+console.log(reference)
+
+
+//array data-type 
+let fruites : string[]
+fruites = ['apple','mango','goava']
+console.log(fruites)
+
+let marks : number[]
+marks = [25,45,50,35]
+console.log(marks)
+
+let mixed : any[]
+mixed = ['rakib',true,24]
+console.log(mixed)
+
+//union way
+let stringOrNumber : (string | number)[]
+stringOrNumber = ['rakib',22]
+
+//tupel way
+let tupleStringOrNumber : [string,number]
+tupleStringOrNumber = ['riyad',22]
+
 
 //void
 function hello() :void {
