7b10b2ed51392f41df0b692b419e06817a0a5013
rirakib <rirrakib10@gmail.com>
2023-09-10 07:51:24 -0700

    data-type

---
+++ data-type/index.js
@@ -0,0 +1,17 @@
+//built in data type 
+var username;
+var age;
+var isActive;
+username = "rakib";
+age = 25;
+isActive = true;
+//void
+function hello() {
+    console.log('hello world');
+}
+//number return 
+function sum(a, b) {
+    return a + b;
+}
+hello();
+sum(10, 20); //30
---
+++ data-type/index.ts
@@ -0,0 +1,26 @@
+//built in data type 
+
+let username : string
+let age : number
+let isActive : boolean
+
+username ="rakib"
+age = 25
+isActive = true 
+
+//void
+function hello() :void {
+    console.log('hello world')
+}
+
+//number return 
+function sum(a : number, b:number) : number {
+    return a+b
+}
+
+hello()
+
+sum(10,20) //30
+
+
+
