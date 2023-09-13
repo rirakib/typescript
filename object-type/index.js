45c8399b86e2b562929c07c8a060f857cf93ee35
rirakib <rirrakib10@gmail.com>
2023-09-13 10:07:46 -0700

    object data type

---
+++ object-type/index.js
@@ -0,0 +1,6 @@
+var users;
+users = [];
+var user;
+user = { name: 'rakib', roll: 25 };
+users.push(user);
+console.log(users);
---
+++ object-type/index.ts
@@ -0,0 +1,8 @@
+let users: object[]
+users = []
+
+let user : {name : string; roll : number; age?: number}
+    user = {name : 'rakib', roll : 25}
+
+users.push(user)
+console.log(users)
